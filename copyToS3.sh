0e7053a0bad3515d26c45534cb6439b933d30418
Sergio Sanchez <trukise@gmail.com>
2018-01-23 23:00:56 +0100

    added square bracket to code

--- copyToS3.sh
+++ copyToS3.sh
@@ -23,9 +23,9 @@ else
     S3_BUCKET=$2
     echo "S3 Bucket: $S3_BUCKET"
 
-    if  aws s3 ls $S3_BUCKET ; then
+    if [[ aws s3 ls $S3_BUCKET ]] ; then
         echo "Copying logs files to S3://logfiles"
-        if aws s3 cp $LOCAL_FOLDER $S3_BUCKET --recursive ; then
+        if [[ aws s3 cp $LOCAL_FOLDER $S3_BUCKET --recursive ]] ; then
             echo "Copied !!"
             echo "Removing files"
             rm -rf "$LOCAL_FOLDER"*
