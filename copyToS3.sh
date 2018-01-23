c749e0a0841949934964faa52bd704c525c3e097
Sergio Sanchez <trukise@gmail.com>
2018-01-23 22:58:02 +0100

    Added display_help function

--- copyToS3.sh
+++ copyToS3.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
+#
+# Script that take two arguments, first the files to copy
+# and second, the bucket to copy them
+
+# Note: Its necessary to install AWS CLI and configure
+# credentials in AWS CLI (aws configure)
 
 
-LOCAL_FOLDER=$1
-echo "Local folder: $LOCAL_FOLDER"
-S3_BUCKET=$2
-echo "S3 Bucket: $S3_BUCKET"
+display_help() {
+    echo "Usage: $0 <source folder> <S3 bucket>"
+    echo
+    echo "   The command is executed with --recursive flag "
 
-if  aws s3 ls $S3_BUCKET ; then
-    echo "Copying logs files to S3://logfiles"
-    if aws s3 cp $LOCAL_FOLDER $S3_BUCKET --recursive ; then
-        echo "Copied !!"
-        echo "Removing files"
-        rm -rf "$LOCAL_FOLDER"*
-    else
-        echo "OHHH Failed copy to S3 bucket"
-    fi
+    exit 1
+}
+
+if [[ $1 == "--help" ]] ; then
+    display_help
 else
-    echo "Bucket doesnt exists"
+    LOCAL_FOLDER=$1
+    echo "Local folder: $LOCAL_FOLDER"
+    S3_BUCKET=$2
+    echo "S3 Bucket: $S3_BUCKET"
+
+    if  aws s3 ls $S3_BUCKET ; then
+        echo "Copying logs files to S3://logfiles"
+        if aws s3 cp $LOCAL_FOLDER $S3_BUCKET --recursive ; then
+            echo "Copied !!"
+            echo "Removing files"
+            rm -rf "$LOCAL_FOLDER"*
+        else
+            echo "OHHH Failed copy to S3 bucket"
+        fi
+    else
+        echo "Bucket doesnt exists"
+    fi
 fi
 
 
